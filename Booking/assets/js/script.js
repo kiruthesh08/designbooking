d80d9750f57b89ab04923243551c3383ee91b3f5
Kiru <37779457+kiruthesh08@users.noreply.github.com>
2023-11-02 17:00:00 +0000

    Add files via upload

---
+++ Booking/assets/js/script.js
@@ -0,0 +1,25 @@
+// for footer carousal
+const carouselContent = document.querySelector(".carousel-content");
+const paragraphs = carouselContent.querySelectorAll("p");
+let currentIndex = 0;
+
+function changeParagraph() {
+  paragraphs[currentIndex].style.display = "none";
+  currentIndex = (currentIndex + 1) % paragraphs.length;
+  paragraphs[currentIndex].style.display = "block";
+}
+
+changeParagraph();
+
+setInterval(changeParagraph, 6000);
+
+//   for sidenav
+const sidenav = document.querySelector(".sidenav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 45) {
+    sidenav.style.top = "0";
+  } else {
+    sidenav.style.top = "77px";
+  }
+});
